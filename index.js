8dbea2b2c0ae6c3814f93e8ceab1b91c49247c4e
AYOMIKUN <AYOMIKUN@DESKTOP-VV1E89Q.com>
2022-02-26 14:40:49 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let birthYear = 1988;
+console.log(birthYear);
+
+let futureYear = 2028
+console.log(futureYear);
+
+let result = futureYear - birthYear 
+ let alternateResult = result - 1
+  
+ console.log('i will be either '+ alternateResult + ' or ' + result + ' in ' + futureYear);
